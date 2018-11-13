caa14345791e0079033b0804ec9ecfb8364b52e5
Jason0123 <ss104113023@khvs.tc.edu.tw>
2018-11-14 01:07:05 +0800

    add new

---
+++ modeldisplay/Assets/scripts/exit.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exit : MonoBehaviour {
+    public void doquit(){
+        Debug.Log("Player quit the game");
+        Application.Quit();
+    }
+}
